9b06903f1bd4238e595835611d2be8f7c8b8b914
brammont <brayanmazuera1@gmail.com>
2023-05-18 15:02:10 +0800

    adding rest-framework and corsheaders

--- api/settings.py
+++ api/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "taskApi"
+    "corsheaders"
+    "rest_framework",
+    "taskApi",
+    
 ]
 
 MIDDLEWARE = [
