c6c21795417b2329c7737d567f0ebeee7bc0072b
Brayam Mazuera <brayanmazuera1@gmail.com>
2023-08-26 15:44:31 +0800

    restarting the project

--- taskApi/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializaer import TaskSerializers
-from .models import Task
-
-
-class TaskView(viewsets.ModelViewSet):
-    serializer_class = TaskSerializers
-    queryset = Task.objects.all()
