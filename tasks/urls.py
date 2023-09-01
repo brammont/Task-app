d8175cd370f97399196110e155e51881fcd5ef0f
Brayam Mazuera <brayanmazuera1@gmail.com>
2023-09-01 17:30:05 +0800

    adding documentation for the api

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
 from tasks import views
 
 #api version
@@ -7,4 +8,5 @@ router = routers.DefaultRouter()
 router.register(r"tasks", views.TaskView, "tasks")
 urlpatterns = [
     path("api/v1/", include(router.urls)),
+    path('docs/', include_docs_urls(title='Tasks API')),
 ]
