ff78484d0795664e60c90ebb181ce0ecba9db533
Brayam Mazuera <brayanmazuera1@gmail.com>
2023-09-01 18:05:34 +0800

    create navigation component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,11 @@ import {
 } from 'react-router-dom'
 import { TaskFormPage } from './pages/TaskFormPage';
 import { TaskPage } from './pages/TaskPage';
+import { Navigation } from './components/Navigation';
 function App() {
   return (
     <BrowserRouter>
+      <Navigation/>
       <Routes>
         <Route path='/' element={<Navigate to="/tasks" />} />
         <Route path='/tasks' element={<TaskPage />} />
